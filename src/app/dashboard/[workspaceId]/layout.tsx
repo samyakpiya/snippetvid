5fe2460ea08de8482c8570dca90243b94be598fa
Samyak Piya <samyakpiya@gmail.com>
2024-10-25 17:24:59 -0400

    feat: set up React Query and create custom hooks for user search and data mutation

---
+++ src/app/dashboard/[workspaceId]/layout.tsx
@@ -0,0 +1,67 @@
+import { getNotifications, onAuthenticateUser } from "@/actions/user";
+import {
+  getAllUserVideos,
+  getWorkspaceFolders,
+  getWorkspaces,
+  verifyAccessToWorkspace,
+} from "@/actions/workspace";
+import { redirect } from "next/navigation";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import React from "react";
+import Sidebar from "@/components/global/sidebar";
+
+type Props = {
+  params: { workspaceId: string };
+  children: React.ReactNode;
+};
+
+const Layout = async ({ params: { workspaceId }, children }: Props) => {
+  const auth = await onAuthenticateUser();
+
+  if (!auth.user?.workspace) redirect("/auth/sign-in");
+  if (!auth.user.workspace.length) redirect("/auth/sign-in");
+
+  const hasAccess = await verifyAccessToWorkspace(workspaceId);
+
+  if (hasAccess.status !== 200) {
+    redirect(`/dashboard/${auth.user.workspace[0].id}`);
+  }
+
+  if (!hasAccess.data?.workspace) return null;
+
+  const query = new QueryClient();
+
+  await query.prefetchQuery({
+    queryKey: ["workspace-folders"],
+    queryFn: () => getWorkspaceFolders(workspaceId),
+  });
+
+  await query.prefetchQuery({
+    queryKey: ["user-videos"],
+    queryFn: () => getAllUserVideos(workspaceId),
+  });
+
+  await query.prefetchQuery({
+    queryKey: ["user-workspaces"],
+    queryFn: () => getWorkspaces(),
+  });
+
+  await query.prefetchQuery({
+    queryKey: ["user-notifications"],
+    queryFn: () => getNotifications(),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(query)}>
+      <div className="flex h-screen w-screen">
+        <Sidebar activeWorkspaceId={workspaceId} />
+      </div>
+    </HydrationBoundary>
+  );
+};
+
+export default Layout;
---
+++ src/components/global/search/index.tsx
@@ -0,0 +1,83 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useMutationData } from "@/hooks/useMutationData";
+import { useSearch } from "@/hooks/useSearch";
+import { User } from "lucide-react";
+import React from "react";
+import Loader from "../loader";
+
+type Props = {
+  workspaceId: string;
+};
+
+const Search = ({ workspaceId }: Props) => {
+  const { query, onSearchQuery, isFetching, onUsers } = useSearch(
+    "get-users",
+    "USERS"
+  );
+
+  // WIP: Wire up sending invitations
+  // WIP: Invite button in UI
+  // const { mutate, isPending } = useMutationData(
+  //   ["invite-member"],
+  //  (data: {receiverId: string; email: string}) => {
+
+  //  },
+  //   "get-users"
+  // );
+
+  return (
+    <div className="flex flex-col gap-y-5">
+      <Input
+        onChange={onSearchQuery}
+        value={query}
+        className="bg-transparent border-2 outline-none"
+        placeholder="Search for your users..."
+        type="text"
+      />
+
+      {isFetching ? (
+        <div className="flex flex-col gapy-2">
+          <Skeleton className="w-full h-10 rounded-xl" />
+        </div>
+      ) : !onUsers ? (
+        <p className="text-center text-sm text-[#a4a4a4]">No Users Found.</p>
+      ) : (
+        <div>
+          {onUsers.map((user) => (
+            <div
+              key={user.id}
+              className="flex gap-x-3 items-cetner border-2 w-full p-3 rounded-xl"
+            >
+              <Avatar>
+                <AvatarImage src={user.image ?? ""} />
+                <AvatarFallback>
+                  <User />
+                </AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col items-start">
+                <h3 className="text-bold text-lg capitalize">
+                  {user.firstname} {user.lastname}
+                </h3>
+                <p className="text-xs lowercase bg-white px-2 rounded-lg text-[#1e1e1e]">
+                  {user.subscription?.plan}
+                </p>
+              </div>
+              <div className="flex-1 flex justify-end items-center">
+                <Button onClick={() => {}} className="w-5/12 font-bold">
+                  <Loader state={false} color="#000">
+                    Invite
+                  </Loader>
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
